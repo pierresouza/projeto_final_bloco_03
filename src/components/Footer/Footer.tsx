622ac2d5494f97afd60645cc1ac483db72c65920
pierresouza <pierre.s3@hotmail.com>
2024-09-04 09:40:46 -0300

    fix: change name from in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <>
       <div className="flex justify-center bg-slate-900 text-white">
         <div className="container flex flex-col items-center py-4">
-          <p className="text-xl font-bold">Blog pessoal Generation | Copyright: {data}</p>
+          <p className="text-xl font-bold">Farmaconde | Copyright: {data}</p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
             <LinkedinLogo size={48} weight="bold" />
